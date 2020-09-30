881e8a373164dafdeb9109490445965eba7c8d68
c1ph3rcr4ck3r <72099871+c1ph3rcr4ck3r@users.noreply.github.com>
2020-09-30 05:51:34 -0400

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,10 @@
+echo "This Tool Made By @c1ph3r_cr4ck3r"
+echo "The Tool Will Install Know"
+echo "+------------------------+"
+echo "|        Installing      | "
+echo "+------------------------+"
+sudo apt install python
+https://github.com/c1ph3rcr4ck3r/c1ph3r_cr4ck3r/
+echo "                     +--------------------------+"
+echo "                     |Thank For Your Downloading|"
+echo "                     +--------------------------+"
